e194128ba491df10a61e4b91bf70d1b05177a10c
ASHIQANWARK <ashiqanwar777@gmail.com>
2025-03-19 12:16:52 +0530

    team commit

--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -6,7 +6,7 @@ import { FaInstagram, FaFacebook, FaLinkedin, FaXTwitter } from "react-icons/fa6
 import shaazImage from "../assets/images/Shaaz.jpg";
 import shradhaImage from "../assets/images/shradha.jpg";
 import royImage from "../assets/images/Roy.jpg";
-import rijuwalImage from "../assets/images/Rizwan.JPG";
+
 
 
 
@@ -14,7 +14,7 @@ const team = [
   { name: "Shaaz meharoof", role: "CHAIRMAN", image: shaazImage },
   {name: "Shradha .S", role: "Chief Accountant", image: shradhaImage},
   {name: "Roy", role: "Sales Director", image: royImage},
-  {name: "Rizwan", role: "Sales Manager ", image: rijuwalImage}
+
 
 
 ];
