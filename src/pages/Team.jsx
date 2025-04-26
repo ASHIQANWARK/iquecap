a996a36397dec00a57dcc7f20c7bdd76b10cd5cd
ASHIQANWARK <ashiqanwar777@gmail.com>
2025-04-26 14:42:31 +0530

    initial commit

--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -6,10 +6,46 @@ import royImage from "../assets/images/Roy.jpg";
 import rizwanImage from "../assets/images/rizwan-img.jpg";
 
 const team = [
-  { name: "Shaaz Bin Maharoof", role: "Founder & CEO", image: shaazImage },
-  { name: "Shradha .S", role: "CFO", image: shradhaImage },
-  { name: "Roy Alexander", role: "Senior Sales Director", image: royImage },
-  { name: "Rizwan", role: "Senior Sales Manager", image: rizwanImage },
+  {
+    name: "Shaaz Bin Maharoof",
+    role: "Founder & CEO",
+    image: shaazImage,
+    socials: {
+      instagram: "https://www.instagram.com/shaazbinmaharoof?igsh=MXVpaXplZTRkdGRiNA==",
+      linkedin: "https://www.linkedin.com/in/shaaz-bin-maharoof-0a0463279?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+      facebook: "https://www.facebook.com/profile.php?id=61572517916783",
+    },
+  },
+  {
+    name: "Shradha .S",
+    role: "CFO",
+    image: shradhaImage,
+    socials: {
+      instagram: "https://www.instagram.com/ique.cap?igsh=b212OWl4eTZvMWt1",
+      linkedin: "ttps://www.linkedin.com/company/ique-cap/",
+      facebook: "https://www.facebook.com/profile.php?id=61572517916783",
+    },
+  },
+  {
+    name: "Roy Alexander",
+    role: "Senior Sales Director",
+    image: royImage,
+    socials: {
+      instagram: "https://www.instagram.com/ique.cap?igsh=b212OWl4eTZvMWt1",
+      linkedin: "ttps://www.linkedin.com/company/ique-cap/",
+      facebook: "https://www.facebook.com/profile.php?id=61572517916783",
+    },
+  },
+  {
+    name: "Rizwan",
+    role: "Senior Sales Manager",
+    image: rizwanImage,
+    socials: {
+      instagram: "https://www.instagram.com/ique.cap?igsh=b212OWl4eTZvMWt1",
+      linkedin: "ttps://www.linkedin.com/company/ique-cap/",
+      facebook: "https://www.facebook.com/profile.php?id=61572517916783",
+    },
+  },
 ];
 
 const Team = () => {
@@ -42,16 +78,36 @@ const Team = () => {
                 <p className="text-gray-200 text-sm font-sans text-center">{member.role}</p>
 
                 <div className="flex space-x-3 mt-3">
-                  <a href="https://www.instagram.com/ique.cap?igsh=b212OWl4eTZvMWt1" className="text-white hover:text-[#E1306C] transition" target="_blank" rel="noopener noreferrer">
-                    <FaInstagram size={20} />
-                  </a>
-                  
-                  <a href="https://www.linkedin.com/company/ique-cap/" className="text-white hover:text-[#0077B5] transition" target="_blank" rel="noopener noreferrer">
-                    <FaLinkedin size={20} />
-                  </a>
-                  <a href="https://www.facebook.com/profile.php?id=61572517916783" className="text-white hover:text-[#1877F2] transition" target="_blank" rel="noopener noreferrer">
-                    <FaFacebook size={20} />
-                  </a>
+                  {member.socials.instagram && (
+                    <a
+                      href={member.socials.instagram}
+                      className="text-white hover:text-[#E1306C] transition"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <FaInstagram size={20} />
+                    </a>
+                  )}
+                  {member.socials.linkedin && (
+                    <a
+                      href={member.socials.linkedin}
+                      className="text-white hover:text-[#0077B5] transition"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <FaLinkedin size={20} />
+                    </a>
+                  )}
+                  {member.socials.facebook && (
+                    <a
+                      href={member.socials.facebook}
+                      className="text-white hover:text-[#1877F2] transition"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <FaFacebook size={20} />
+                    </a>
+                  )}
                 </div>
               </div>
             </div>
